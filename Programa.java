75796d10fbe431285f4fc7e71ceaa8d4983da197
ricardo <ricardoejr@gmail.com>
2013-08-26 12:05:54 -0300

    mais uma mensagem

--- Programa.java
+++ Programa.java
@@ -2,5 +2,6 @@ public class Programa
 {
    public static void main(String[] args){
       System.out.println("Exemplo de teste de uso do git");
+      System.out.println("Trabalhando com branches");
    }
 }
